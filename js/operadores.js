3b36a0cd1dc8f6ecd1a4697c5acec379449e6b76
carlos horacio lopez <carloshoracio.lopez@comafi.com.ar>
2023-10-10 19:58:53 -0300

    add funciones + operadores logicos + if/else

--- js/operadores.js
+++ js/operadores.js
@@ -6,18 +6,33 @@
     / division
     * multiplicacion
 */
-function sumar() {
-    let valor1 = document.getElementById('valor1').value;//por ser input
-    let valor2 = document.getElementById('valor2').value;
-    let resultado = Number(valor1) + Number(valor2);
-    //alert(resultado);
-    document.getElementById('res').innerHTML = resultado;//por que es div
+function sumar(valor1,valor2) {    
+    return Number(valor1) + Number(valor2);
 }
 
-function restar() {
-    let valor1 = document.getElementById('valor1').value;//por ser input
-    let valor2 = document.getElementById('valor2').value;
-    let resultado = Number(valor1) - Number(valor2);
-    //alert(resultado);
-    document.getElementById('res').innerHTML = resultado;//por que es div
+function restar(valor1,valor2) {
+    return Number(valor1) - Number(valor2);        
+}
+function calcular(operacion) {
+    let valor1 = getValueById('valor1');//1
+    let valor2 = getValueById('valor2');//
+    let resultado = '';
+    if(valor1 !== '' && valor2 !== '') {
+        //quiero saber que operacion realizar
+        if(operacion === '+') {
+            //invoco la funcion sumar
+            resultado = sumar(valor1,valor2);
+        }else {
+            resultado = restar(valor1,valor2);
+        }
+    }
+    actualizarContenido('res',resultado);
+}
+
+function getValueById(id) {
+    //defino una constante
+    return document.getElementById(id).value;
+}
+function actualizarContenido(id,nuevoContenido) {
+    document.getElementById(id).innerHTML = nuevoContenido;
 }
